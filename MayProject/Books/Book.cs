68bca05b99427a75abfc7905defae3ceedc47baf
PacificNorthwest <crimea165@gmail.com>
2017-02-05 17:20:12 +0200

    Project added

---
+++ MayProject/Books/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MayProject.Books
+{
+    [Serializable]
+    class Book
+    {
+        public string BookName { get; set; }
+        public List<Character> Characters { get; set; }
+        public List<Location> Locations { get; set; }
+        public List<Note> Notes { get; set; }
+        public List<Chapter> Chapters { get; set; }
+
+        public Book(string bookName)
+        {
+            BookName = bookName;
+            Characters = new List<Character>();
+            Locations = new List<Location>();
+            Notes = new List<Note>();
+            Chapters = new List<Chapter>();
+        }
+
+    }
+}
---
+++ MayProject/XmlManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+using MayProject.Books;
+
+namespace MayProject
+{
+    class XmlManager
+    {
+        public static void Save(object obj)
+        {
+            XmlSerializer xml = new XmlSerializer(obj.GetType());
+            using (FileStream file = new FileStream("data.xml", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                xml.Serialize(file, obj);
+            }
+        }
+
+        public static Book Load()
+        {
+            XmlSerializer xml = new XmlSerializer(typeof(Book));
+            using (FileStream file = new FileStream("data.xml", FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                return xml.Deserialize(file) as Book ;
+            }
+        }
+    }
+}
